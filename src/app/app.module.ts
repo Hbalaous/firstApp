77193438f5c4870103e530017e2621c87e71f69f
balaoushamzah <balaoushamzah@gmail.com>
2021-03-03 14:18:25 +0000

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from "@angular/forms"
+
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    NavbarComponent,
+    SidebarComponent,
+    IntroComponent,
+    ContentComponent,
+    FooterComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/intro/intro.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.css']
+})
+export class IntroComponent implements OnInit {
+
+  editable = false
+  username = {
+    name: '',
+    active: false
+  }
+
+  names = [
+    { name: "Kamal", active: true },
+    { name: "Hanane", active: true },
+    { name: "Mahmoud", active: false },
+    { name: "Nawfal", active: true }
+  ]
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addName() {
+    this.names = [...this.names, this.username]
+    this.username = {
+      name: '',
+      active: false
+    }
+    this.editable = true
+  }
+
+}
